28eec1dd6d7c9aaa8c1c61739b81e5265a454632
Aman-kushwaha <76246298+Amaanku@users.noreply.github.com>
2025-02-24 17:55:10 +0530

    Python file

---
+++ stegovault.py
@@ -0,0 +1,55 @@
+import cv2
+import os
+import string
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+# Hide the root window
+Tk().withdraw()
+
+# Open file dialog to select an image
+image_path = askopenfilename(title="Select an image", filetypes=[("Image files", "*.jpg *.jpeg *.png")])
+if not image_path:
+    print("No image selected.")
+    exit()
+
+img = cv2.imread(image_path)  # Use the selected image path
+
+msg = input("Enter secret message:")
+password = input("Enter a passcode:")
+
+d = {}
+c = {}
+
+for i in range(255):
+    d[chr(i)] = i
+    c[i] = chr(i)
+
+m = 0
+n = 0
+z = 0
+
+for i in range(len(msg)):
+    img[n, m, z] = d[msg[i]]
+    n = n + 1
+    m = m + 1
+    z = (z + 1) % 3
+
+cv2.imwrite("encryptedImage.jpg", img)
+os.system("start encryptedImage.jpg")  # Use 'start' to open the image on Windows
+
+message = ""
+n = 0
+m = 0
+z = 0
+
+pas = input("Enter passcode for Decryption")
+if password == pas:
+    for i in range(len(msg)):
+        message = message + c[img[n, m, z]]
+        n = n + 1
+        m = m + 1
+        z = (z + 1) % 3
+    print("Decryption message:", message)
+else:
+    print("YOU ARE NOT auth")
